7abef68ab12117175f329e258ee7c399a70d661f
Caleb <83906743+jonathancaleb@users.noreply.github.com>
2022-04-22 14:54:57 +0300

    redux

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return ( <
+        div className = "col-md-12 bg-dark py-2" >
+        <
+        nav className = "navbar bg-dark navbar-dark" >
+        <
+        Link to = { "/" }
+        className = "navbar-brand ml-5" >
+        Staff Management <
+        /Link> <
+        /nav> <
+        /div>
+    );
+};
+
+export default Navbar;
